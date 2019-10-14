b7ebd34784caaa40c9bcb9a2475282b740ca0e0c
Higg83 <wllmsundberg@gmail.com>
2019-10-14 08:22:20 +0200

    Changed inject to look for custom attribute instad of css class

--- inject.py
+++ inject.py
@@ -6,16 +6,16 @@ def _get_soup(html_path):
 	f.close()
 	return soup
 
-def _get_replaced_soup(html_path, html_class, string):
+def _get_replaced_soup(html_path, attribute, string):
 	soup = _get_soup(html_path)
-	tags = soup.find_all(class_ = html_class)
+	tags = soup.find_all(attrs={str(attribute):True})
 	for tag in tags:
 		tag.string = string
 	return soup
 
 # if output_path is None then it overwrites the original file
-def inject(input_path, html_class, string, output_path = None):
-	soup = _get_replaced_soup(input_path, html_class, string)
+def inject(input_path, attribute, string, output_path = None):
+	soup = _get_replaced_soup(input_path, attribute, string)
 	if output_path == None:
 		output_path = input_path
 	f = open(output_path, "w")
